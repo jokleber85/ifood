fba12b3e9a40f8b259ef061778f24d49bfe16b12
John Kleber <jo.kleber85@gmail.com>
2021-06-11 22:00:37 -0300

    Implementação voucher, background do SaibaMais e manutenção do carrousel

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -1,8 +1,27 @@
-$('.owl-carousel').owlCarousel({
+$('#topRestaurantes').owlCarousel({
     loop:true,
     margin:35,
     nav: true,
-    navText: [""],
+    navText: ['<span class="icone"><i class="small material-icons">chevron_left</i></span>','<span class="icone"><i class="small material-icons">chevron_right</i></span>'],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#topMercados').owlCarousel({
+    loop:true,
+    margin:35,
+    nav: true,
+    navText: ['<span class="icone"><i class="small material-icons">chevron_left</i></span>','<span class="icone"><i class="small material-icons">chevron_right</i></span>'],
     dots:false,
     responsive:{
         0:{
