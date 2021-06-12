4202901e40c516bcddc3d3a9475d7e3bf22763df
John Kleber <jo.kleber85@gmail.com>
2021-06-12 14:29:07 -0300

    Adicionado linha do header-bottom, closed do voucher e os itens do melhores mercados

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -1,3 +1,20 @@
+function fechar(){
+    const voucher = document.getElementById("voucher");
+    voucher.style.display = "none";
+}
+
+var scrollHeaderBottom = function() {
+  if (window.scrollY >= 140) {
+    $(".header").css("border-bottom", "1px solid #A6A29F");
+    $(".header").css("border-opacity", "0.2");
+    $(".header").css("transition", "all 0.4s");
+} else {
+    $(".header").css("border-bottom", "1px solid #F7F7F7");
+  }
+};
+
+window.addEventListener("scroll", scrollHeaderBottom);
+
 $('#topRestaurantes').owlCarousel({
     loop:true,
     margin:35,
